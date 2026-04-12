b15d43aa3043b2ecad778a4a29755046367cc18a
IvaBatur <161125041+IvaBatur@users.noreply.github.com>
2026-04-12 11:45:54 +0200

    Add files via upload

---
+++ checkpoint2/hotel_db.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE hotel_db;
+USE hotel_db; 
+SELECT 
+    c.name AS country,
+    h.hotel_name AS hotel,
+    m.type AS meal,
+    b.lead_time,
+    b.arrival_date_year,
+    b.arrival_date_month,
+    b.stays_in_weekend_nights,
+    b.stays_in_week_nights,
+    b.adults,
+    b.children,
+    b.adr,
+    b.market_segment,
+    b.distribution_channel
+    
+FROM booking b
+JOIN country c ON b.country_fk = c.id
+JOIN hotel h ON b.hotel_fk = h.id
+JOIN meal m ON b.meal_fk = m.id
+ORDER BY b.id ASC;
