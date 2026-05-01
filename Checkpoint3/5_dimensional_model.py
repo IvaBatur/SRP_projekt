4d47075d7167118f1a3ad424449db7261896c70c
IvaBatur <161125041+IvaBatur@users.noreply.github.com>
2026-05-01 15:00:38 +0200

    Add new dimensions and update FactBooking schema

--- Checkpoint3/5_dimensional_model.py
+++ Checkpoint3/5_dimensional_model.py
@@ -10,7 +10,7 @@ Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
 
-# 2. DIMENZIJE ( 5 različitih dimenzija)
+# 2. DIMENZIJE
 
 class DimHotel(Base):
     __tablename__ = 'dim_hotel'
@@ -28,7 +28,7 @@ class DimCustomer(Base):
     customer_type = Column(String(50))
     is_repeated_guest = Column(Integer)
     
-    # Sporo mijenjajuće dimenzije
+    # Sporo mijenjajuće dimenzije 
     version = Column(Integer, default=1) 
     date_from = Column(DateTime, default=lambda: datetime.now(timezone.utc))
     date_to = Column(DateTime, nullable=True)
@@ -38,24 +38,35 @@ class DimLocation(Base):
     __table_args__ = {'schema': 'dw'}
 
     location_tk = Column(BigInteger, primary_key=True, autoincrement=True)
-    country = Column(String(100)) # Hierarhijska dimenzija - država
+    country = Column(String(100))
+    region = Column(String(100))     
+    continent = Column(String(100))   
 
 class DimMeal(Base):
     __tablename__ = 'dim_meal'
     __table_args__ = {'schema': 'dw'}
 
     meal_tk = Column(BigInteger, primary_key=True, autoincrement=True)
-    meal = Column(String(45))
+    meal = Column(String(45))      
+    meal_description = Column(String(100)) 
+    is_package = Column(Integer)      
+
+class DimStatus(Base):               
+    __tablename__ = 'dim_status'
+    __table_args__ = {'schema': 'dw'}
+
+    status_tk = Column(Integer, primary_key=True, autoincrement=True)
+    reservation_status = Column(String(20)) # 'Canceled', 'Check-Out', 'No-Show'
 
 class DimTime(Base):
     __tablename__ = 'dim_time'
     __table_args__ = {'schema': 'dw'}
 
     time_tk = Column(Integer, primary_key=True, autoincrement=True)
-    arrival_date_year = Column(Integer, nullable=False) # Hierarhijska dimenzija - godina
-    arrival_date_month = Column(String(20), nullable=False) # Hierarhijska dimenzija - mjesec 
-    arrival_date_day_of_month = Column(Integer, nullable=False) # Hierarhijska dimenzija - dan u mjesecu
-    arrival_date_week_number = Column(Integer) # Hierarhijska dimenzija - broj tjedna u godini
+    arrival_date_year = Column(Integer, nullable=False)
+    arrival_date_month = Column(String(20), nullable=False)
+    arrival_date_day_of_month = Column(Integer, nullable=False)
+    arrival_date_week_number = Column(Integer)
     is_weekend = Column(Integer)
 
 # 3. TABLICA ČINJENICA 
@@ -65,23 +76,22 @@ class FactBooking(Base):
     __table_args__ = {'schema': 'dw'}
 
     booking_tk = Column(BigInteger, primary_key=True, autoincrement=True)
-    adr = Column(Float) # kvantitativna mjera - average daily rate
-    lead_time = Column(Integer) # kvantitativna mjera - broj dana između rezervacije i dolaska
-    total_nights = Column(Integer) # kvantitativna mjera - ukupni broj noćenja (stays_in_weekend_nights + stays_in_week_nights)
-    adults = Column(Integer) # kvantitativna mjera - broj odraslih osoba
-    children = Column(Integer) # kvantitativna mjera - broj djece
-    is_canceled = Column(Integer) # kvantitativna mjera - 1 ako je rezervacija otkazana, 0 inače
-    reservation_status = Column(String(20)) #degenerirana dimenzija - status rezervacije ('Canceled', 'Check-Out', 'No-Show')
+    adr = Column(Float)
+    lead_time = Column(Integer)
+    total_nights = Column(Integer)
+    adults = Column(Integer)
+    children = Column(Integer)
+    is_canceled = Column(Integer)
 
     time_tk = Column(Integer, ForeignKey('dw.dim_time.time_tk'))
     customer_tk = Column(BigInteger, ForeignKey('dw.dim_customer.customer_tk'))
     location_tk = Column(BigInteger, ForeignKey('dw.dim_location.location_tk'))
     hotel_tk = Column(BigInteger, ForeignKey('dw.dim_hotel.hotel_tk'))
     meal_tk = Column(BigInteger, ForeignKey('dw.dim_meal.meal_tk'))
+    status_tk = Column(Integer, ForeignKey('dw.dim_status.status_tk'))
 
 #  4. KREIRANJE TABLICA
 
 if __name__ == "__main__":
-
     Base.metadata.create_all(engine)
     print(" Dimenzijski model (star schema) kreiran uspješno.")
